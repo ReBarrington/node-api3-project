bf74e9a1b18140a369f4010338c884030842202d
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-08 15:59:22 -0400

    postRouter.js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,22 +1,96 @@
 const express = require('express');
-
+const Posts = require('./postDb');
 const router = express.Router();
 
+// GET /api/posts... Returns an array of all the post objects contained in the database:
 router.get('/', (req, res) => {
-  // do your magic!
-});
+  Posts.get(req.query)
+  .then((posts) => {
+      res.status(200).json({ queryString: req.query, posts });
+  })
+  .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error retrieving posts."})
+  })
+})
 
+
+// GET /api/posts/:id... Returns the post object with the specified id:
 router.get('/:id', (req, res) => {
-  // do your magic!
-});
+  Posts.getById(req.params.id)
+  .then((post) => {
+      if (post) {
+          res.status(200).json(post);
+      } else {
+          res.status(404).json({ message: "Post not found."})
+      }
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error retrieving the post."})
+  })
+})
 
+// DELETE /api/posts/:id... Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
 router.delete('/:id', (req, res) => {
-  // do your magic!
-});
+  let id = req.params.id;
+  Posts.getById(id)
+  .then((post) => {
+      // post is either post with matching id or empty array...
+      if (post.length) {
+          try {
+              Posts.remove(id)
+              .then(numRemaining => {
+                  res.status(200).json(post)
+              })
+          // change database to no longer include deleted post.
+          }
+          catch(err) {
+              res.status(500).json({ errorMessage: "The post could not be removed."})
+          }
+      } else {
+          res.status(404).json({ message: "Post not found."})
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ message: "Sorry, something went wrong."})
+  })
+})
 
+// PUT /api/posts/:id.... Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
 router.put('/:id', (req, res) => {
-  // do your magic!
-});
+  let id = req.params.id;
+  let changes = req.body;
+  // make sure not missing title or contents
+  if (!req.body.title || !req.body.contents) {
+      res.status(400).json({ message: "Missing title and/or contents."})
+  } else {
+      Posts.getById(id)
+      .then((post) => {
+          // post is either post with matching id or empty array...
+          if (post.length) {
+              try {
+                  // if post is valid: 
+                  Posts.update(id, changes)
+                  .then(count => {
+                      if (count > 0) {
+                          // updated correctly:
+                          res.status(200).json(changes)
+                      }
+                  })
+              }
+              catch(err) {
+                  res.status(500).json({ errorMessage: "The post could not be removed."})
+              }
+          } else {
+              res.status(404).json({ message: "Post not found."})
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ message: "Sorry, something went wrong."})
+      })
+  }
+})
 
 // custom middleware
 
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 
+const postRouter = require("./posts/postRouter.js");
+
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use(express.json()); // built-in middleware, no need to install it
 //custom middleware
 server.use(logger);
+
+//endpoints
+server.use("/api/posts", postRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
+
+  res.send(`
+    <h2>Lambda Hubs API</h2>
+    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    `);
+});
 
 function logger(req, res, next) {
   // logs request method, request url, and a timestamp to the console
