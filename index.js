a299bb4297db3430e29bde3e7d58e499f289c79d
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-09 15:31:06 -0400

    create app on heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+// const port = 5000; This won't work because what if another application is running on this port?
+// make the port dynamic... so now it will get the port from the environment object/heroku (process.env).
+const port = process.env.PORT|| 5000; // use dynamic port, or if undefined (using locally), just use 5000 as default. 
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
